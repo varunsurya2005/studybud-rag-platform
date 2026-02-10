c582757dfc783198a1798a8c8b8d3599186734bf
Varun Surya <varunsurya_cherukuri@srmap.edu.in>
2026-02-10 16:35:36 +0530

    JWT auth working with protected routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+
 
 const connectDB = require("./config/db");
 
@@ -10,6 +12,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
 
 
 app.get("/", (req, res) => {
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,69 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      userId: user._id,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// LOGIN
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      role: user.role,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
